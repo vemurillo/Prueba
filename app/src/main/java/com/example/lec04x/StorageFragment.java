52477dd43de99bb1047e2c0779e8ee5a8e0d09a5
Victor <victorwwe350@hotmail.com>
2019-07-01 21:18:56 -0600

    prueba 4

--- app/src/main/java/com/example/lec04x/StorageFragment.java
+++ app/src/main/java/com/example/lec04x/StorageFragment.java
@@ -35,7 +35,7 @@ public class StorageFragment extends Fragment {
     private ImageView imagen;
     private static final int GALLERYINTENT = 1;
     private StorageReference mStorageRef;
-    private ImageView prueba;
+    private ImageView prueba4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
